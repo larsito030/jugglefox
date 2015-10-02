0a983952a0c2a16476a6f6f5e0672a7743fc6265
Lars Jensen <larsjensen@gmx.de>
2015-10-02 11:30:46 +0200

    Update config.php

--- config.php
+++ config.php
@@ -9,25 +9,7 @@ define("PATH","/Juggleflash");
 define("LOCATION", "");
 error_reporting(0);
 
-//WEB SERVER CONFIGURATION:
-/*
-Strato:
 
-$user = "U2242253";
-$pass = "hamburg12";
-$db = new PDO("mysql:host=rdbms.strato.de;dbname=DB2242253", $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-define("PATH","/mockup-elternfreund.com");
-define("LOCATION", "http://mockup-elternfreund.com/");
-*/
-//All inkl:
-/*
-$user = "d01f35a7";
-$pass = "Hamburg12";
-$db = new PDO("mysql:host=larsitogames.com;dbname=d01f35a7", $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-define("PATH","/larsitogames.com");
-define("LOCATION", "http://larsitogames.com/");
-
-*/
 
 
 
